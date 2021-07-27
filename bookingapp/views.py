85445c2e8d75af57f60b0e4220513bfa5bc383fd
Rubyroy12 <you@example.com>
2021-07-27 20:52:51 +0300

    fetch properties

--- bookingapp/views.py
+++ bookingapp/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from .models import Property
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,HttpResponseRedirect
+from .models import Property,Reviews
 from django.contrib.auth.models import User
+from .forms import ReviewsForm
+
 
 # Create your views here.
 def index(request):
@@ -19,8 +22,26 @@ def profile(request,username):
     return render(request,'profile.html')
 
 def details(request,name):
+    # current_user= request.user
     home = Property.objects.get(name=name)
-    
+    # comment=Reviews.objects.filter(id)
+    if request.method == 'POST':
+        form = ReviewsForm(request.POST,request.FILES)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.property=property
+            comment.user = request.user.profile
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form= ReviewsForm()
+    params={
+        'home':home,
+        'form': form,
+        # 'comment':comment,
+
+        }
+    return render(request,'details.html',params)
 
 
 
