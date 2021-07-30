576a2d7f4ef69ce7ead4cc33d21c3d307496bf80
Rubyroy12 <you@example.com>
2021-07-30 10:58:09 +0300

    deploy to heroku

--- property/settings.py
+++ property/settings.py
@@ -102,16 +102,16 @@ WSGI_APPLICATION = 'property.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'property1',
-#         'USER': 'ibrahim',
-#         'PASSWORD':'secret',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'property',
+        'USER': 'ibrahim',
+        'PASSWORD':'secret',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
+    }
+}
 
 
 # Password validation
