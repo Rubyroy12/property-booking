02959bdc3a68658e958d9134284340577923ca9f
Rubyroy12 <ibrakipz7@gmail.com>
2021-09-13 15:24:21 +0300

    create database

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/ibrahim/Documents/python/django-projects/property-boooking/venv/bin/python3
+#!/home/ibrahim/Documents/projects/python/property-booking/venv/bin/python3
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
